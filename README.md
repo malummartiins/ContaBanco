bd17aba41b9592950a409447c8adf1ce103f8aa6
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-17 17:48:38 -0300

    novo projeto java básico

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sintaxe - Desafio
-Vamos exercitar todo o conteúdo apresentado no módulo de Sintaxe codificando o seguinte cenário.
+Vamos exercitar todo o conteúdo apresentado no módulo de Sintaxe codificando o seguinte cenário:
 
 * Crie o projeto ContaBanco que receberá dados via terminal contendo as características de conta em banco conforme atributos abaixo:
 * Dentro do projeto, crie a classe ContaTerminal.java para realizar toda a codificação do nosso programa.
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,15 +11,13 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o número: ");
         int num = scan.nextInt();
 
-
         System.out.println("Digite o nome do cliente:");
         String nomeCliente = scan1.nextLine();
 
         System.out.println("Informe o saldo:");
         float saldo = scan.nextFloat();
 
-        System.out.println("Olá, " + nomeCliente +  " obrigado por criar uma conta em nosso banco, sua agência é, " + agencia +  " conta " + num + "e seu saldo "+ saldo + " já está disponível para saque");
-        
+        System.out.println("Olá, " + nomeCliente +  " obrigado por criar uma conta em nosso banco, sua agência é, " + agencia +  " conta " + num + "e seu saldo "+ saldo + " já está disponível para saque"); 
 
     }
 }
